83239cac5ee6826cf5281fe070a419c73a9147a3
ezgi <ezginrucay@gmail.com>
2019-02-15 13:56:25 +0300

    version 0.0.4 multi chat room added

--- Web-Socket/server.js
+++ Web-Socket/server.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
 const port = 3000;
-
+var roomno;
 
 
 app.get('/', (req, res) => {
@@ -15,21 +15,37 @@ http.listen(port, function () {
 const connections = [];
 
 io.sockets.on('connection', (socket) => {
+
+
+  socket.on('select room', (data) => {
+    //if (io.nsps['/'].adapter.rooms["room-" + data] && io.nsps['/'].adapter.rooms["room-" + data].length > Roomcapacity) roomno++;
+
+    console.log("selected room id " + data);
+    socket.join("room-" + data);
+    io.sockets.in("room-" + data).emit('connectToRoom', { message: "You are in " + data +" .room"});
+    roomno = data;
+  });
+
+
+
+
   connections.push(socket);
   console.log(' %s sockets is connected', connections.length);
-  
-  socket.on('disconnect', () => {
-    connections.splice(connections.indexOf(socket), 1);
-  });
+
 
   socket.on('sending message', (message) => {
     console.log('Message is received :', message);
-    io.sockets.emit('new message', { message: message });
+    io.sockets.in("room-" + roomno).emit('new message', { message: message });
   });
 
 socket.on('broadcast',(message)=>{
   socket.broadcast.emit('broadcastMessage', { message: 'hi everyone online client count is' + connections.length });
 });
  
+
+
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+  });
 
 });
