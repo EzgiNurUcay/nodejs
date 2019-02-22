ddfcad491849774a84275b5ecd590722f3eb4c2d
ezginrucayg@gmail.com <ezginrucay@gmail.com>
2019-02-22 15:22:53 +0300

    user name added & message format designed

--- Web-Socket/server.js
+++ Web-Socket/server.js
@@ -3,6 +3,9 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
 const port = 3000;
+var info = [];
+
+
 
 
 app.get('/', (req, res) => {
@@ -19,9 +22,19 @@ io.sockets.on('connection', (socket) => {
   socket.on('select room', (data) => {
 
     console.log("selected room id " + data);
+    console.log("socket id is:" + socket.id);
     socket.join("room-" + data);
-    io.sockets.in(Object.keys(io.sockets.adapter.sids[socket.id])[1]).emit('connectToRoom', { message: "You are in '" + data + "' room" });
+    io.sockets.in("room-" + data).emit('connect room', { message: "You are in '" + data + "' room" });
     //console.log("room name is", Object.keys(io.sockets.adapter.sids[socket.id])[1]); shows room name.
+
+
+  });
+
+  socket.on('user name defined', (data) => {
+
+    info.push({ socketid: socket.id, username: data });
+    console.log(info);
+    io.sockets.in(socket.id).emit('connect user', { message: "User Name " + data });
   });
 
 
@@ -29,13 +42,27 @@ io.sockets.on('connection', (socket) => {
   console.log(' %s sockets is connected', connections.length);
 
 
-  socket.on('sending message', (message) => {
-    console.log('Message is " %s " received from %s:', message, Object.keys(io.sockets.adapter.sids[socket.id])[1]);
-    io.sockets.in(Object.keys(io.sockets.adapter.sids[socket.id])[1]).emit('new message', { message: message });
+  socket.on('send message', (message) => {
+    //console.log("searched id is", socket.id);
+    //console.log("check ", Object.keys(io.sockets.adapter.sids[socket.id]));
+    //console.log("match", info.find(o => o.socketid === socket.id));
+    for (let i = 0; i < info.length; i++) {
+      if (info[i].socketid != socket.id) {
+        if (Object.keys(io.sockets.adapter.sids[socket.id])[1] === Object.keys(io.sockets.adapter.sids[info[i].socketid])[1]) {
+          io.sockets.in(info[i].socketid, Object.keys(io.sockets.adapter.sids[socket.id])[1]).emit('incomming message', { message: info.find(o => o.socketid === socket.id).username + " says: " + message });
+        }
+      }
+    }
+
+
+    io.sockets.in(socket.id).emit('outgoing message', { message: info.find(o => o.socketid === socket.id).username + " says: " + message });
   });
 
   socket.on('broadcast', (message) => {
+    console.log("socket id is:" + socket.id);
     socket.broadcast.emit('broadcastMessage', { message: 'hi everyone online client count is' + connections.length });
+
+
   });
 
 
